b5fc503fdd9d90e7f00bf938c2ea41f5e943a7ed
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-08-12 20:23:24 +0100

    chore: Update database connection string variable name

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ mod services;
 use services::{create_user_article, fetch_user_articles, fetch_users};
 
 pub struct AppState {
-    db: Pool<Postgres>
+    db: Pool<Postgres>,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let database_url = std::env::var("CONNECTION_STRING").expect("CONNECTION_STRING must be set");
     let pool = PgPoolOptions::new()
         .max_connections(5)
         .connect(&database_url)
