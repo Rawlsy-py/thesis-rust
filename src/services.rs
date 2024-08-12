91f679517f1d059e353a46a9412074f566813cfc
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-08-12 20:37:18 +0100

    chore: Remove unused code and comments

--- src/services.rs
+++ src/services.rs
@@ -41,38 +41,38 @@ pub async fn fetch_users(state: Data<AppState>) -> impl Responder {
     }
 }
 
-#[get("/users/{id}/articles")]
-pub async fn fetch_user_articles(state: Data<AppState>, path: Path<i32>) -> impl Responder {
-    let id: i32 = path.into_inner();
-    // format!("GET /users/{id}/articles")
+// #[get("/users/{id}/articles")]
+// pub async fn fetch_user_articles(state: Data<AppState>, path: Path<i32>) -> impl Responder {
+//     let id: i32 = path.into_inner();
+//     // format!("GET /users/{id}/articles")
 
-    match sqlx::query_as::<_, Article>(
-        "SELECT id, title, content, created_by FROM articles WHERE created_by = $1"
-    )
-        .bind(id)
-        .fetch_all(&state.db)
-        .await
-    {
-        Ok(articles) => HttpResponse::Ok().json(articles),
-        Err(_) => HttpResponse::NotFound().json("No articles found"),
-    }
-}
+//     match sqlx::query_as::<_, Article>(
+//         "SELECT id, title, content, created_by FROM articles WHERE created_by = $1"
+//     )
+//         .bind(id)
+//         .fetch_all(&state.db)
+//         .await
+//     {
+//         Ok(articles) => HttpResponse::Ok().json(articles),
+//         Err(_) => HttpResponse::NotFound().json("No articles found"),
+//     }
+// }
 
-#[post("/users/{id}/articles")]
-pub async fn create_user_article(state: Data<AppState>, path: Path<i32>, body: Json<CreateArticleBody>) -> impl Responder {
-    let id: i32 = path.into_inner();
-    // format!("POST /users/{id}/articles")
+// #[post("/users/{id}/articles")]
+// pub async fn create_user_article(state: Data<AppState>, path: Path<i32>, body: Json<CreateArticleBody>) -> impl Responder {
+//     let id: i32 = path.into_inner();
+//     // format!("POST /users/{id}/articles")
 
-    match sqlx::query_as::<_, Article>(
-        "INSERT INTO articles (title, content, created_by) VALUES ($1, $2, $3) RETURNING id, title, content, created_by"
-    )
-        .bind(body.title.to_string())
-        .bind(body.content.to_string())
-        .bind(id)
-        .fetch_one(&state.db)
-        .await
-    {
-        Ok(article) => HttpResponse::Ok().json(article),
-        Err(_) => HttpResponse::InternalServerError().json("Failed to create user article"),
-    }
-}
+//     match sqlx::query_as::<_, Article>(
+//         "INSERT INTO articles (title, content, created_by) VALUES ($1, $2, $3) RETURNING id, title, content, created_by"
+//     )
+//         .bind(body.title.to_string())
+//         .bind(body.content.to_string())
+//         .bind(id)
+//         .fetch_one(&state.db)
+//         .await
+//     {
+//         Ok(article) => HttpResponse::Ok().json(article),
+//         Err(_) => HttpResponse::InternalServerError().json("Failed to create user article"),
+//     }
+// }
