5c27cb38fcc1127969f6c504e95ec873e5769c92
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-08-19 19:48:27 +0100

    chore: Update log dependency version and add error logging in fetch_users

--- src/services.rs
+++ src/services.rs
@@ -1,5 +1,6 @@
 use crate::AppState;
 use actix_web::{get, web::Data, HttpResponse, Responder};
+use log::error;
 use serde::Serialize;
 use sqlx::{self, FromRow};
 
@@ -12,13 +13,20 @@ struct Users {
 
 #[get("/users")]
 pub async fn fetch_users(state: Data<AppState>) -> impl Responder {
-    // "GET /users".to_string()
-
     match sqlx::query_as::<_, Users>("SELECT * FROM USERS;")
         .fetch_all(&state.db)
         .await
     {
-        Ok(users) => HttpResponse::Ok().json(users),
-        Err(_) => HttpResponse::NotFound().json("No users found"),
+        Ok(users) => {
+            if users.is_empty() {
+                HttpResponse::NotFound().json("No users found")
+            } else {
+                HttpResponse::Ok().json(users)
+            }
+        }
+        Err(e) => {
+            error!("Database query failed: {:?}", e);
+            HttpResponse::InternalServerError().json("Internal Server Error")
+        }
     }
 }
