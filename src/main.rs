2cc5a1d08e777ac56e5f2bf4351b594a50112b37
Rawlsy-py <christopher_m_mitchell@live.co.uk>
2024-08-22 09:40:22 +0100

    chore: Add index route and response in services.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use dotenv::dotenv;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod services;
-use services::fetch_users;
+use services::{fetch_users, index};
 
 pub struct AppState {
     db: Pool<Postgres>,
@@ -12,19 +12,28 @@ pub struct AppState {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    let connection_string =
-        std::env::var("CONNECTION_STRING").expect("CONNECTION_STRING must be set");
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&connection_string)
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let pool = match PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
         .await
-        .expect("Error building a connection pool");
+    {
+        Ok(pool) => {
+            println!("✅Connection to the database is successful!");
+            pool
+        }
+        Err(err) => {
+            println!("🔥 Failed to connect to the database: {:?}", err);
+            std::process::exit(1);
+        }
+    };
 
-    println!("Server running at http://127.0.0.1:8080/users");
+    println!("Server running at http://127.0.0.1:8080/");
 
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(AppState { db: pool.clone() }))
+            .service(index)
             .service(fetch_users)
     })
     .bind(("127.0.0.1", 8080))?
